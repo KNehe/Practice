4bafe1c1cdb89813901d40be77785b0b726b3c1a
= <kamolunehemiah@gmail.com>
2020-07-09 15:21:12 +0300

    binary tree traversal in javascript

--- Js/BinaryTree.js
+++ Js/BinaryTree.js
@@ -95,19 +95,107 @@ class BTree{
         }
         return false;
     }
+
+    inOrder = () =>{
+        let array = [];
+        
+        let node = this.root;
+        
+       const recur = (node) =>{
+            if(node !=null){
+               if(node.left) recur(node.left)
+               array.push(node.data)
+               if(node.right) recur(node.right)
+            }else{
+                return null;
+            }
+        };
+
+        recur(node);
+
+        return array;
+    };
+
+    preOrder = () =>{
+        let array = [];
+        
+        let node = this.root;
+        
+       const recur = (node) =>{
+            if(node !=null){
+                array.push(node.data)
+               if(node.left) recur(node.left)
+               if(node.right) recur(node.right)
+            }else{
+                return null;
+            }
+        };
+
+        recur(node);
+
+        return array;
+    };
+
+    postOrder = () =>{
+        let array = [];
+        
+        let node = this.root;
+        
+       const recur = (node) =>{
+            if(node !=null){
+               if(node.left) recur(node.left)
+               if(node.right) recur(node.right)
+               array.push(node.data)
+            }else{
+                return null;
+            }
+        };
+
+        recur(node);
+
+        return array;
+    };
+
+    
+    levelOrder = () =>{
+        let array = [];
+        let queue = [];
+
+        if(this.root != null){
+            queue.push(this.root);
+            while(queue.length > 0){
+                let node = queue.shift();
+                array.push(node.data);
+                if(node.left != null) queue.push(node.left);
+                if(node.right != null) queue.push(node.right);
+            }
+
+        }else{
+            return null;
+        }
+        return array;
+    };
+    
+    
 }
 
 const binaryTree = new BTree();
-binaryTree.add(2);
-binaryTree.add(1);
-binaryTree.add(0);
+binaryTree.add(9);
 binaryTree.add(4);
-binaryTree.add(5);
+binaryTree.add(17);
 binaryTree.add(3);
-binaryTree.add(2);
-binaryTree.add(-2);
+binaryTree.add(6);
+binaryTree.add(22);
+binaryTree.add(5);
+binaryTree.add(7);
+binaryTree.add(20);
 
-console.log("min",binaryTree.min());
-console.log("max",binaryTree.max());
-console.log("find",binaryTree.find(72));
-console.log("ispresent",binaryTree.isPresent(5));
+
+// console.log("min",binaryTree.min());
+// console.log("max",binaryTree.max());
+// console.log("find",binaryTree.find(72));
+// console.log("ispresent",binaryTree.isPresent(5));
+console.log("inOrder", binaryTree.inOrder())
+console.log("preOrder", binaryTree.preOrder())
+console.log("postOrder", binaryTree.postOrder())
+console.log("levelOrder", binaryTree.levelOrder())
